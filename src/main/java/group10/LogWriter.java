30f454a1d3fe4e10b6d9501932f5cdcc8a4cbe2e
Hannes Hallqvist <hannes.hallqvist@hotmail.com>
2023-02-07 22:16:19 +0100

    better timestamps in log

--- src/main/java/group10/LogWriter.java
+++ src/main/java/group10/LogWriter.java
@@ -2,19 +2,46 @@ package group10;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class LogWriter {
     File file;
 
     public LogWriter() {
-        file = new File("history/" + new Date() + ".html");
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY-MM-dd_HH:mm:ss");
+        String filename = date.format(formatter);
+
+        file = new File("history/" + filename + ".html");
+        try {
+            FileWriter writer = new FileWriter(file, true);
+            writer.append("<ul>");
+            writer.close();
+        } catch (Exception e) {
+            return;
+        }
     }
 
     public void write(String message) {
         try {
             FileWriter writer = new FileWriter(file, true);
-            writer.append("<p>" + message + "</p>");
+
+            LocalDateTime date = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String time = date.format(formatter);
+
+            writer.append("<p>" + "[" + time + "] " + message + "</p>");
+            writer.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+    public void close() {
+        try {
+            FileWriter writer = new FileWriter(file, true);
+            writer.append("</ul>");
             writer.close();
         } catch (Exception e) {
             return;
