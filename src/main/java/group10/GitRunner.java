c63330d3f1e4735f20aeac35860aa260f5f86efb
Tobias Mattsson <tmatts@kth.se>
2023-02-07 11:05:59 +0100

    Fix cloneRepo so that build folder is deleted when cloning anew

--- src/main/java/group10/GitRunner.java
+++ src/main/java/group10/GitRunner.java
@@ -6,11 +6,22 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 public class GitRunner {
+    /**
+     * Clones a git repository using the given URL and branch name, and puts it
+     * all in a folder named "/build"
+     * @param repoURL the URL for the repo
+     * @param branch the branch name to clone from
+     * @throws GitAPIException if clone fails
+     */
     public static void cloneRepo(String repoURL, String branch) throws GitAPIException {
-        // create new folder to clone repo in
+        // create new folder to clone repo in (if already exists: delete)
         File repoDir = new File("/build");
+        if (repoDir.exists()) {
+            repoDir.delete();
+        }
         repoDir.mkdir();
 
+        // clone the repo from the given branch
         Git.cloneRepository()
                 .setURI(repoURL)
                 .setDirectory(new File("/build"))
