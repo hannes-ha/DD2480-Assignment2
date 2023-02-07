cc253f87e26dc2176a83aae0502ceaffec600953
tayloh <21138680+tayloh@users.noreply.github.com>
2023-02-07 09:15:57 +0100

    Functionality/6 parse requests (#10)
    * Add skeleton structure for request handling
    
    * Add json library
    
    * Add logic for POST body to JSON
    
    * Add util for check if commit event
    
    * Add tests for isCommitEvent
    
    * Add check for commit/push event
    
    * Add impl. and tests for getBranch
    
    * Add getRepositoryName and tests
    
    * Add getCloneURL and tests
    
    * Reformat (standard intellij format)
    
    * Add dummy output on CI runs
    
    * Remove target parameter (unused)
    
    * Add convertToString from BufferedReader
    
    * Fix bug in parsing request
    
    * Fix bug with retrieving clone address

--- src/main/java/group10/App.java
+++ src/main/java/group10/App.java
@@ -1,15 +1,13 @@
 package group10;
- 
+
 import org.eclipse.jetty.server.Server;
 
 
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
-        System.out.println( "Starting CI server..." );
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting CI server...");
         Server server = new Server(8080);
-        server.setHandler(new ContinuousIntegrationServer()); 
+        server.setHandler(new ContinuousIntegrationServer());
         server.start();
         server.join();
     }
---
+++ src/test/java/group10/UtilTest.java
@@ -0,0 +1,111 @@
+package group10;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import org.json.simple.JSONObject;
+
+public class UtilTest {
+
+    /**
+     * Asserts true
+     * Should return true since the payload contains a head_commit property
+     * and its commit id is not null
+     */
+    @Test
+    public void testUtil_isCommitEvent_Positive() {
+        JSONObject payload = new JSONObject();
+        JSONObject headCommitObject = new JSONObject();
+
+        headCommitObject.put("id", "b383e670849aa57543c54c0c074ea6362bdaae99");
+        payload.put("head_commit", headCommitObject);
+
+        boolean result = Util.isCommitEvent(payload);
+        assertTrue(result);
+    }
+
+    /**
+     * Asserts false
+     * Should return false since the payload contains a head_commit property
+     * but its commit id is null
+     */
+    @Test
+    public void testUtil_isCommitEvent_Negative_1() {
+        JSONObject payload = new JSONObject();
+        JSONObject headCommitObject = new JSONObject();
+
+        headCommitObject.put("id", null);
+        payload.put("head_commit", headCommitObject);
+
+        boolean result = Util.isCommitEvent(payload);
+        assertFalse(result);
+    }
+
+
+    /**
+     * Asserts false
+     * Should return false since the payload does not contain
+     * a head_commit property
+     */
+    @Test
+    public void testUtil_isCommitEvent_Negative_2() {
+        JSONObject payload = new JSONObject();
+        JSONObject headCommitObject = new JSONObject();
+
+        headCommitObject.put("id", "182391");
+        payload.put("some_prop", headCommitObject);
+
+        boolean result = Util.isCommitEvent(payload);
+        assertFalse(result);
+    }
+
+    /**
+     * Assert equals (expected = "main")
+     * Should return "main" since the payload refers
+     * to the main branch
+     */
+    @Test
+    public void testUtil_getBranch() {
+        JSONObject payload = new JSONObject();
+        payload.put("ref", "refs/heads/main");
+
+        String result = Util.getBranch(payload);
+        assertEquals("main", result);
+    }
+
+    /**
+     * Assert equals (expected = "test-repo-name")
+     * Should return "test-repo-name" since the payload is tied
+     * to the repository with name "test-repo-name"
+     */
+    @Test
+    public void testUtil_getRepositoryName() {
+        JSONObject payload = new JSONObject();
+        JSONObject repository = new JSONObject();
+
+        repository.put("name", "test-repo-name");
+        payload.put("repository", repository);
+
+        String result = Util.getRepositoryName(payload);
+        assertEquals("test-repo-name", result);
+    }
+
+    /**
+     * Assert equals (expected = "https://github.com/hannes-ha/DD2480-Assignment2.git")
+     * Should return "https://github.com/hannes-ha/DD2480-Assignment2.git" since payload
+     * has repository url "https://github.com/hannes-ha/DD2480-Assignment2"
+     */
+    @Test
+    public void testUtil_getCloneURL() {
+        JSONObject payload = new JSONObject();
+        JSONObject repository = new JSONObject();
+
+        repository.put("url", "https://github.com/hannes-ha/DD2480-Assignment2");
+        payload.put("repository", repository);
+
+        String result = Util.getCloneURL(payload);
+        assertEquals("https://github.com/hannes-ha/DD2480-Assignment2.git", result);
+    }
+
+}
