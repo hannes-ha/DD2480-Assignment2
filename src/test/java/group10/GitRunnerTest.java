67eef9f53d82f429e1fcb171b5a8163be7056329
Tobias Mattsson <tmatts@kth.se>
2023-02-07 11:45:12 +0100

    Added test for GitRunner

--- src/test/java/group10/GitRunnerTest.java
+++ src/test/java/group10/GitRunnerTest.java
@@ -1,13 +1,31 @@
 package group10;
 
-import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.GitAPIException;;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GitRunnerTest {
+    /**
+     * Assert throws.
+     * Try cloning a repo with invalid parameters: should throw GitAPIException.
+     */
     @Test
     public void testGitRunner_Invalid() {
         assertThrows(GitAPIException.class, () -> GitRunner.cloneRepo("", ""));
+    }
+
+    /**
+     * Assert true.
+     * Try cloning an existing repo, and check if the folder has been created.
+     * @throws GitAPIException
+     */
+    @Test
+    public void testGitRunner_Positive() throws GitAPIException {
+        GitRunner.cloneRepo("https://github.com/hannes-ha/DD2480-Assignment2.git", "main");
+        File repoDir = new File("./build");
+        assertTrue(repoDir.exists());
     }
 }
