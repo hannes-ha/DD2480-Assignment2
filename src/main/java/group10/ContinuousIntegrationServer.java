7d78e77fea8c0227e3da5b90fabfab670c965aa2
hannes-ha <63145826+hannes-ha@users.noreply.github.com>
2023-02-08 11:04:31 +0100

    Feat/21 notification flow (#24)
    * Add utility for getting commit hash
    
    * Integrate buildhistory and commit status
    
    * Add history to gitignore
    
    * Fix bug with buildstatus
    
    * fix branch extraction bug
    
    ---------
    
    Co-authored-by: estolpe <estolpe@kth.se>

--- src/main/java/group10/ContinuousIntegrationServer.java
+++ src/main/java/group10/ContinuousIntegrationServer.java
@@ -173,14 +173,24 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         BuildStatus buildStatus = BuildStatus.PENDING;
         BuildStatus testsStatus = BuildStatus.PENDING;
 
+        String commitHash = Util.getCommitHash(payload);
+        CommitStatusHandler statusHandler = new CommitStatusHandler();
+        statusHandler.setStatus(commitHash, "pending");
+
+        LogWriter logWriter = new LogWriter();
+        logWriter.write("Commit hash: " + commitHash);
+
         // Running clone
         System.out.println("Running git clone on " + Util.getCloneURL(payload) + " branch " + Util.getBranch(payload));
+        logWriter.write("Running git clone on " + Util.getCloneURL(payload) + " branch " + Util.getBranch(payload));
+
         boolean cloneSuccess = true;
         try {
             GitRunner.cloneRepo(Util.getCloneURL(payload), Util.getBranch(payload), "build");
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
+            logWriter.write(e.getMessage());
             cloneSuccess = false;
         }
 
@@ -196,7 +206,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
 
             // Running mvn
             System.out.println("Running mvn compile...");
-
+            logWriter.write("Running mvn compile...");
 
             MavenRunner mavenRunner = new MavenRunner("./build");
             finalStatus = mavenRunner.runMvnCompile();
@@ -206,10 +216,11 @@ public class ContinuousIntegrationServer extends AbstractHandler {
             // If compile succeeded, run tests
             if (finalStatus != BuildStatus.BUILD_FAILED) {
                 System.out.println("Running mvn test...");
+                logWriter.write("Running mvn test...");
                 testsStatus = mavenRunner.runMvnTest();
+                finalStatus = testsStatus;
             }
         }
-
 
         System.out.println("____________________________________________________________");
         System.out.println("Results:");
@@ -217,6 +228,18 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         System.out.println(buildStatus);
         System.out.println(testsStatus);
         System.out.println("____________________________________________________________");
+
+        logWriter.write("Results:");
+        logWriter.write(cloneStatus.toString());
+        logWriter.write(buildStatus.toString());
+        logWriter.write(testsStatus.toString());
+        logWriter.close();
+
+        if (finalStatus == BuildStatus.TESTS_SUCCEEDED) {
+            statusHandler.setStatus(commitHash, "success");
+        } else {
+            statusHandler.setStatus(commitHash, "failure");
+        }
 
     }
 
